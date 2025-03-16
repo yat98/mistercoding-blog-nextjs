be5a483fb96ec087a8eb79abe4166132daaeb8b2
Hidayat Chandra <hidayatchandra08@gmail.com>
2025-03-16 22:56:12 +0800

    feat: wrap children in LayoutContainer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { CProvider } from "@/cproviders";
+import LayoutContainer from "./layout-container";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <CProvider>
-          {children}
+          <LayoutContainer>
+            {children}
+          </LayoutContainer>
         </CProvider>
       </body>
     </html>
