35de870bdb9889c603b002c9313312984c142628
Hidayat Chandra <hidayatchandra08@gmail.com>
2025-03-17 23:39:07 +0800

    feat: add posts service get data

---
+++ src/services/posts.service.ts
@@ -0,0 +1,27 @@
+import { Post } from "@/models/post"
+import api from "@/utils/api"
+
+export const getPosts = async () => {
+  return await api.get<Post[]>("/api/posts", {
+    params: {
+      populate: "*",
+    },
+    transformResponse: (res) => {
+      const resData = JSON.parse(res)
+      return resData.data
+    },
+  })
+}
+
+export const getPost = async (slug: string) => {
+  return await api.get<Post>(`/api/posts`, {
+    params: {
+      populate: "*",
+      "filters[slug][$eq]": slug,
+    },
+    transformResponse: (res) => {
+      const resData = JSON.parse(res)
+      return resData.data[0] ?? {}
+    },
+  })
+}
