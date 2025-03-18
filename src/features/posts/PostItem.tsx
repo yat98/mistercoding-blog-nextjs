6b0d413011413964e295f0ce6f7da1560349f3d1
Hidayat Chandra <hidayatchandra08@gmail.com>
2025-03-18 23:12:52 +0800

    feat: add PostItem component

---
+++ src/features/posts/PostItem.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { Post } from '@/models/post';
+import { media } from '@/utils/media';
+import { Box, CardBody, CardRoot, Image, Text } from '@chakra-ui/react';
+
+interface Props {
+  post: Post;
+}
+
+export default function PostItem({ post }: Props) {
+  return (
+    <CardRoot
+      my={8}
+      overflow={'hidden'}
+      flexDirection={{ base: 'column', md: 'row' }}
+      alignItems={'center'}
+    >
+      <Image
+        maxW={{ base: '100%', md: '150px' }}
+        objectFit="cover"
+        src={media(post.thumbnail[0].url)}
+        alt={post.title}
+        width={150}
+        height={150}
+      />
+      <Box>
+        <CardBody>
+          <Text fontWeight={'bold'} fontSize={'lg'} color={'gray.600'}>
+            {post.title}
+          </Text>
+          <Text fontSize={'md'} color={'gray.600'}>
+            {post.short_description}
+          </Text>
+        </CardBody>
+      </Box>
+    </CardRoot>
+  );
+}
