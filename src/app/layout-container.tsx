13619c897c74c54f26dfca40503ab362a5c005a5
Hidayat Chandra <hidayatchandra08@gmail.com>
2025-03-18 23:55:13 +0800

    feat: add color mode toggle component

--- src/app/layout-container.tsx
+++ src/app/layout-container.tsx
@@ -1,34 +1,40 @@
-"use client"
+'use client';
 
-import { useColorModeValue } from "@/components/ui/color-mode"
-import { Box, DrawerRoot, DrawerContent } from "@chakra-ui/react"
-import { useState } from "react"
-import { MobileNav, SideBarContent } from "./sidebar"
+import { useColorModeValue } from '@/components/chakra-ui/ColorMode';
+import { Box, DrawerRoot, DrawerContent } from '@chakra-ui/react';
+import { useState } from 'react';
+import { MobileNav, SideBarContent } from './sidebar';
 
-export default function LayoutContainer({ children }: { children: React.ReactNode }) {
-  const [open, setOpen] = useState<boolean>(false)
+export default function LayoutContainer({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [open, setOpen] = useState<boolean>(false);
 
   return (
-    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
+    <Box minH={'100vh'} bg={useColorModeValue('gray.100', 'gray.900')}>
       <SideBarContent
         onClose={() => setOpen(false)}
-        display={{ base: "none", md: "block" }}
+        display={{ base: 'none', md: 'block' }}
       />
       <DrawerRoot
         open={open}
-        placement={"start"}
+        placement={'start'}
         onOpenChange={() => setOpen(false)}
-        size="full">
-          <DrawerContent>
-            <SideBarContent
-              onClose={() => setOpen(false)}
-            />
-          </DrawerContent>
+        size="full"
+      >
+        <DrawerContent>
+          <SideBarContent onClose={() => setOpen(false)} />
+        </DrawerContent>
       </DrawerRoot>
-      <MobileNav display={{ base: 'flex', md: 'none' }} onOpen={() => setOpen(true)} />
+      <MobileNav
+        display={{ base: 'flex', md: 'none' }}
+        onOpen={() => setOpen(true)}
+      />
       <Box ml={{ base: 0, md: 60 }} p={4}>
         {children}
       </Box>
     </Box>
-  )
+  );
 }
