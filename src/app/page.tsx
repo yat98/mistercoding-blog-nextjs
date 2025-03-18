458b4a1007035ffa1cf01ed5472273c2387540a8
Hidayat Chandra <hidayatchandra08@gmail.com>
2025-03-18 23:56:00 +0800

    feat: add PageTitle component and caching page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import PageTitle from '@/components/ui/PageTitle';
 import Posts from '@/features/posts/Posts';
+
+// Cache for 1 hour
+export const revalidate = 3600;
 
 export default function Home() {
   return (
     <>
-      <h1>Posts</h1>
+      <PageTitle title="Post" />
       <Posts />
     </>
   );
