a90d3b5389ca1482e25ff6358c2f693716a8fe24
Hidayat Chandra <hidayatchandra08@gmail.com>
2025-03-23 21:50:23 +0800

    feat: update contact page UI

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,34 @@
-import { Heading } from "@chakra-ui/react";
+'use client';
+
+import PageTitle from '@/components/ui/PageTitle';
+import { CardBody, CardRoot } from '@chakra-ui/react';
+import { AiOutlineMail, AiOutlinePhone } from 'react-icons/ai';
 
 export default function Page() {
   return (
     <>
-      <Heading color={"black"} size={"xl"}>Contact</Heading>  
+      <div className="my-4!">
+        <PageTitle title="Contact" />
+        <div className="my-4!">
+          <CardRoot>
+            <CardBody>
+              <p className="mb-4!">For consultations. Please contact me at:</p>
+              <div className="flex! items-center!">
+                <span>
+                  <AiOutlineMail />
+                </span>
+                <p className="ml-4!">loremipsum@mail.com</p>
+              </div>
+              <div className="flex! items-center! mt-3!">
+                <span>
+                  <AiOutlinePhone />
+                </span>
+                <p className="ml-4!">(+62) 812-345-6789</p>
+              </div>
+            </CardBody>
+          </CardRoot>
+        </div>
+      </div>
     </>
-  )
+  );
 }
